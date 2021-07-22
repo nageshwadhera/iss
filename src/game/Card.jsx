8dba348259ebf31e76c3581f7cc3365fae4679fc
Nagesh Kumar <nageshwadhera@gmail.com>
2021-07-22 20:03:54 +0530

    initial commit

--- src/game/Card.jsx
+++ src/game/Card.jsx
@@ -1,48 +1,60 @@
-import React from 'react';
+import React from "react";
 
-import './Card.css';
+import "./Card.css";
 
-export default function Card({currentItem, uncovered, onSelectProperty, selectedProperty, properties, playersTurn = false}) {
-
-    function selectPropertyHandler(prop){
-        if(playersTurn){
-            onSelectProperty(prop);
-        }
+export default function Card({
+  currentItem,
+  uncovered,
+  onSelectProperty,
+  selectedProperty,
+  properties,
+  playersTurn = false,
+}) {
+  function selectPropertyHandler(prop) {
+    if (playersTurn) {
+      onSelectProperty(prop);
     }
+  }
+  /*
+Add some styling for the card component and its table to the Card.css file.
+The Component should look like the screenshot picture in the public folder.
+If the user selects a property from the card the property should be highlighted in yellow.
+(Card.css)
 
-    const front = (
-        <div className="card">
-            <h1>{currentItem.name ? currentItem.name : 'Unbekannt'}</h1>
-            {(
-                <img
-                    alt={currentItem.name}
-                    src={`${process.env.PUBLIC_URL}/placeholder.png`}
-                    height="200"
-                    width="200"
-                />
-            )}
-            <table>
-                <tbody>
-                {Object.keys(properties).map(property => {
-                    const itemProperty = properties[property];
-                    return (
-                        <tr
-                            key={property}
-                            className={selectedProperty === property ? 'active' : ''}
-                            onClick={() => selectPropertyHandler(property)}
-                        >
-                            <td>{itemProperty.label}</td>
-                            <td>
-                                {currentItem[property]}&nbsp;{itemProperty.unit}
-                            </td>
-                        </tr>
-                    );
-                })}
-                </tbody>
-            </table>
-        </div>
-    );
-    const back = <div className="card back" />;
+*/
+  const front = (
+    <div className="card">
+      <h1>{currentItem.name ? currentItem.name : "Unbekannt"}</h1>
+      {
+        <img
+          alt={currentItem.name}
+          src={`${process.env.PUBLIC_URL}/placeholder.png`}
+          height="200"
+          width="200"
+        />
+      }
+      <table>
+        <tbody>
+          {Object.keys(properties).map((property) => {
+            const itemProperty = properties[property];
+            return (
+              <tr
+                key={property}
+                className={selectedProperty === property ? "active" : ""}
+                onClick={() => selectPropertyHandler(property)}
+              >
+                <td>{itemProperty.label}</td>
+                <td>
+                  {currentItem[property]}&nbsp;{itemProperty.unit}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+  const back = <div className="card back" />;
 
-    return <div>{uncovered ? front : back}</div>;
+  return <div>{uncovered ? front : back}</div>;
 }
--- src/game/useCards.jsx
+++ src/game/useCards.jsx
@@ -1,113 +1,139 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import * as _ from 'lodash';
-import update from 'immutability-helper';
+import * as _ from "lodash";
+import update from "immutability-helper";
 
 import Person from "../comparable/Person";
 import Vehicle from "../comparable/Vehicle";
 import Starship from "../comparable/Starship";
 
-export default function useCards(initialComp){
-    const [computerUncovered, setComputerUncovered] = useState(false);
-    const [selectedProperty, setSelectedProperty] = useState('');
-    const [playersTurn, setPlayersTurn] = useState(true);
-    const [player, setPlayer] = useState([]);
-    const [computer, setComputer] = useState([]);
-    const [currComp, setCurrComp] = useState(initialComp);
-    const [loading, setLoading] = useState(false);
+export default function useCards(initialComp) {
+  const [computerUncovered, setComputerUncovered] = useState(false);
+  const [selectedProperty, setSelectedProperty] = useState("");
+  const [playersTurn, setPlayersTurn] = useState(true);
+  const [player, setPlayer] = useState([]);
+  const [computer, setComputer] = useState([]);
+  const [currComp, setCurrComp] = useState(initialComp);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        setLoading(true);
-        getData();
-    }, [currComp])
+  useEffect(() => {
+    setLoading(true);
+    getData();
+  }, [currComp]);
 
-    /**
-     * get async data from external api
-     */
-    function getData(){
-        const url = `https://swapi.dev/api/${currComp}`;
-        {/* TODO Task 2 */}
-        {/* TODO Task 2 */}
+  /**
+   * get async data from external api
+   */
+  /*
+    Implement a async function and use the fetch api or axios 
+    to get data from the starwars api (the url is already there)
+    After the data is fetched call the 'dealCards' function with 
+    the results and set loading to false (useCards.jsx)
+    */
+  async function getData() {
+    const url = `https://swapi.dev/api/${currComp}`;
+    axios.get(url).then((res) => {
+      dealCards(res.data.results);
+      setLoading(false);
+    });
+  }
+
+  useEffect(() => {
+    if (selectedProperty !== "") {
+      setTimeout(() => {
+        compare(selectedProperty);
+      }, 2000);
+    }
+  }, [selectedProperty]);
+
+  useEffect(() => {
+    if (
+      computerUncovered === false &&
+      selectedProperty === "" &&
+      playersTurn === false
+    ) {
+      setTimeout(() => {
+        const prop = selectRandomProperty();
+        play(prop);
+      }, 2000);
+    }
+  }, [computerUncovered, playersTurn, selectedProperty]);
+
+  function compare(prop) {
+    let playersTurnUpdate = playersTurn;
+
+    const firstPlayer = player[0];
+    const firstComputer = computer[0];
+    let playerNew,
+      computerNew = [];
+    {
+      /* TODO Task 4 splice the first card from player and computer */
     }
 
-    useEffect(() => {
-        if(selectedProperty !== ''){
-            setTimeout(() => {
-                compare(selectedProperty);
-            }, 2000)
-        }
-    }, [selectedProperty]);
-
-    useEffect(() => {
-        if(computerUncovered === false
-            && selectedProperty === ''
-            && playersTurn === false
-        ) {
-            setTimeout(() => {
-                const prop = selectRandomProperty();
-                play(prop);
-            }, 2000)
-        }
-    }, [computerUncovered, playersTurn, selectedProperty]);
-
-    function compare(prop){
-        let playersTurnUpdate = playersTurn;
-
-        const firstPlayer = player[0];
-        const firstComputer = computer[0];
-        let playerNew, computerNew = [];
-        {/* TODO Task 4 splice the first card from player and computer */}
-
-        {/* TODO Task 4 */}
-
-        if (firstPlayer[prop] > firstComputer[prop]) {
-            // player wins - player gets computers card
-            playersTurnUpdate = true;
-            {/* TODO Task 4 update players array */}
-
-            {/* TODO Task 4 */}
-
-            if (computerNew.length === 0) {
-                alert('Player wins');
-                return;
-            }
-        } else if (firstPlayer[prop] < firstComputer[prop]) {
-            // computer wins - computer gets players card
-            playersTurnUpdate = false;
-            {/* TODO Task 4 update computers array */}
-
-            {/* TODO Task 4 */}
-
-            if (playerNew.length === 0) {
-                alert('Computer wins');
-                return;
-            }
-        } else {
-            // draw - no card changes
-            {/* TODO Task 4 update players and computers array */}
-
-            {/* TODO Task 4 */}
-        }
-
-        setPlayersTurn(playersTurnUpdate);
-        setSelectedProperty('');
-        setComputerUncovered(false);
-        setPlayer(playerNew);
-        setComputer(computerNew);
+    {
+      /* TODO Task 4 */
     }
 
-    function play(prop){
-        setSelectedProperty(prop);
-        setComputerUncovered(true);
+    if (firstPlayer[prop] > firstComputer[prop]) {
+      // player wins - player gets computers card
+      playersTurnUpdate = true;
+      {
+        /* TODO Task 4 update players array */
+      }
+
+      {
+        /* TODO Task 4 */
+      }
+
+      if (computerNew.length === 0) {
+        alert("Player wins");
+        return;
+      }
+    } else if (firstPlayer[prop] < firstComputer[prop]) {
+      // computer wins - computer gets players card
+      playersTurnUpdate = false;
+      {
+        /* TODO Task 4 update computers array */
+      }
+
+      {
+        /* TODO Task 4 */
+      }
+
+      if (playerNew.length === 0) {
+        alert("Computer wins");
+        return;
+      }
+    } else {
+      // draw - no card changes
+      {
+        /* TODO Task 4 update players and computers array */
+      }
+
+      {
+        /* TODO Task 4 */
+      }
     }
 
-    function selectRandomProperty(){
-        const properties = Object.keys(getProperties());
-        const index = Math.floor(Math.random() * properties.length);
-        return properties[index];
-    }
+    setPlayersTurn(playersTurnUpdate);
+    setSelectedProperty("");
+    setComputerUncovered(false);
+    setPlayer(playerNew);
+    setComputer(computerNew);
+  }
 
-    /**
+  function play(prop) {
+    setSelectedProperty(prop);
+    setComputerUncovered(true);
+  }
+
+  function selectRandomProperty() {
+    const properties = Object.keys(getProperties());
+    const index = Math.floor(Math.random() * properties.length);
+    return properties[index];
+  }
+
+  /**
      * data should be an array of objects for example like this
      * [{
      *      "name": "Luke Skywalker",
@@ -116,87 +142,87 @@ export default function useCards(initialComp){
      * }]
      * @param data
      */
-    function dealCards(data){
-        const newComputer = [];
-        const newPlayer = [];
-        let allCards = [];
-        data.forEach((obj) => {
-            let newCard = null;
-            switch(currComp){
-                case 'people':
-                    newCard = new Person(
-                        obj?.name,
-                        parseInt(obj?.height, 10),
-                        parseInt(obj?.mass, 10),
-                        obj?.films?.length,
-                        obj?.starships?.length,
-                        obj?.vehicles?.length
-                    );
-                    break;
-                case 'starships':
-                    newCard = new Starship(
-                        obj?.name,
-                        obj?.length,
-                        obj?.crew,
-                        obj?.cargo_capacity,
-                        obj?.pilots?.length,
-                        obj?.films?.length,
-                    );
-                    break;
-                case 'vehicles':
-                    newCard = new Vehicle(
-                        obj?.name,
-                        obj?.length,
-                        obj?.crew,
-                        obj?.cost_in_credits,
-                        obj?.pilots?.length,
-                        obj?.films?.length,
-                    );
-                    break;
-            }
-            allCards.push(newCard);
-        })
-        allCards = _.shuffle(allCards);
-        allCards.forEach((card, idx) => {
-            if(idx % 2 === 0){
-                newComputer.push(card);
-            } else {
-                newPlayer.push(card);
-            }
-        })
-        setComputer(newComputer);
-        setPlayer(newPlayer);
-    }
+  function dealCards(data) {
+    const newComputer = [];
+    const newPlayer = [];
+    let allCards = [];
+    data.forEach((obj) => {
+      let newCard = null;
+      switch (currComp) {
+        case "people":
+          newCard = new Person(
+            obj?.name,
+            parseInt(obj?.height, 10),
+            parseInt(obj?.mass, 10),
+            obj?.films?.length,
+            obj?.starships?.length,
+            obj?.vehicles?.length
+          );
+          break;
+        case "starships":
+          newCard = new Starship(
+            obj?.name,
+            obj?.length,
+            obj?.crew,
+            obj?.cargo_capacity,
+            obj?.pilots?.length,
+            obj?.films?.length
+          );
+          break;
+        case "vehicles":
+          newCard = new Vehicle(
+            obj?.name,
+            obj?.length,
+            obj?.crew,
+            obj?.cost_in_credits,
+            obj?.pilots?.length,
+            obj?.films?.length
+          );
+          break;
+      }
+      allCards.push(newCard);
+    });
+    allCards = _.shuffle(allCards);
+    allCards.forEach((card, idx) => {
+      if (idx % 2 === 0) {
+        newComputer.push(card);
+      } else {
+        newPlayer.push(card);
+      }
+    });
+    setComputer(newComputer);
+    setPlayer(newPlayer);
+  }
 
-    function getProperties(){
-        switch (currComp){
-            case 'people':
-                return Person.properties;
-            case 'starships':
-                return Starship.properties;
-            case 'vehicles':
-                return Vehicle.properties;
-            default:
-                return {};
-        }
+  function getProperties() {
+    switch (currComp) {
+      case "people":
+        return Person.properties;
+      case "starships":
+        return Starship.properties;
+      case "vehicles":
+        return Vehicle.properties;
+      default:
+        return {};
     }
+  }
 
-    function reset(){
-        getData();
-        setComputerUncovered(false);
-        setSelectedProperty('');
-    }
+  function reset() {
+    getData();
+    setComputerUncovered(false);
+    setSelectedProperty("");
+  }
 
-    return {
-        computerUncovered,
-        selectedProperty,
-        computer,
-        player,
-        playersTurn,
-        loading,
-        setCurrComp,
-        play,
-        getProperties,
-        reset
-    };
+  return {
+    computerUncovered,
+    selectedProperty,
+    computer,
+    player,
+    playersTurn,
+    loading,
+    setCurrComp,
+    play,
+    getProperties,
+    reset,
+  };
 }
